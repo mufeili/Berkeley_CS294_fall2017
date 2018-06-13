684720062e09b63a0ac2417749264b7476b6fde0
Mufei Li <mufeili1996@gmail.com>
2018-06-13 13:38:05 +0800

    Add files via upload

--- assignments/hw2/code/warmup.sh
+++ assignments/hw2/code/warmup.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -dna -l 1 -s 32
-    --exp_name sb_no_rtg_dna
-python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -rtg -dna -l 1 -s 32
-    --exp_name sb_rtg_dna
-python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -rtg -l 1 -s 32
-    --exp_name sb_rtg_na
-python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -dna -l 1 -s 32
-    --exp_name lb_no_rtg_dna
-python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -rtg -dna -l 1 -s 32
-    --exp_name lb_rtg_dna
-python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -rtg -l 1 -s 32
-    --exp_name lb_rtg_na
+python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -dna -l 1 -s 32 --exp_name sb_no_rtg_dna
+python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -rtg -dna -l 1 -s 32 --exp_name sb_rtg_dna
+python train_pg.py CartPole-v0 -n 100 -b 1000 -e 5 -rtg -l 1 -s 32 --exp_name sb_rtg_na
+python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -dna -l 1 -s 32 --exp_name lb_no_rtg_dna
+python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -rtg -dna -l 1 -s 32 --exp_name lb_rtg_dna
+python train_pg.py CartPole-v0 -n 100 -b 5000 -e 5 -rtg -l 1 -s 32 --exp_name lb_rtg_na
